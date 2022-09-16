31165561d335b79b9d61ea8d31db854a66628332
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-17 00:17:36 +0300

    created Directory: 0x0A-configuration_management added a readme.md file with the dirname as title
    added File: 0-create_a_file.pp
    0. Create a file
    -- Using Puppet, create a file in /tmp.
    
    Requirements:
    
    File path is /tmp/school
    File permission is 0744
    File owner is www-data
    File group is www-data
    File contains I love Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp.
+
+file { '/tmp/school' :
+    ensure  => present,
+    content => 'I love Puppet',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,3 @@
+0x0A-configuration_management
+## Tasks
+0. Create a file
