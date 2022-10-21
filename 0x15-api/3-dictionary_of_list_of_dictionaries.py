0ddff808ed14450f13072088d119ebeb3fa4428d
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-21 15:51:46 +0300

    3. Dictionary of list of dictionaries ------------- sing what you did in the task #0, extend your Python script to export data in the JSON format. ------------------ converted to an executable file ------------------- implemented and created json file

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+what you did in the task #0, extend your
+Python script to export data in the CSV format.
+"""
+
+
+import csv
+import json
+import requests
+from sys import argv
+
+
+def main():
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    all_todos = requests.get(
+        "{}/todos/".format(
+            base_url,
+        )
+    ).json()
+
+    all_employees = requests.get(
+        "{}/users/".format(
+            base_url
+        )
+    ).json()
+
+    json_task_data = []
+    dictionary = {}
+    
+    for employee in all_employees:
+        for todo in all_todos:
+            if todo.get('userId') == employee.get('id'):
+                json_task_data.append(
+                    {
+                        "username": employee.get('username'),
+                        "task": todo.get("title"),
+                        "completed": todo.get('completed')
+                    }
+                )
+                
+        dictionary[employee.get('id')] = json_task_data
+    
+    json_object = json.dumps(dictionary)
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        jsonfile.write(json_object)
+
+
+if __name__ == "__main__":
+    main()
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -5,3 +5,4 @@
 0. Gather data from an API
 1. Export to CSV
 2. Export to JSON
+3. Dictionary of list of dictionaries
