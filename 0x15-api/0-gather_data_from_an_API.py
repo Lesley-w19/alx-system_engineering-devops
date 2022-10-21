63da2c3d235e0fa990aae4ade7db14ecbe857e7e
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-21 14:05:57 +0300

    added identation throughout the lines

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,47 +8,49 @@ about his/her TODO list progress.
 from sys import argv
 import requests
 import urllib
-base_url = "https://jsonplaceholder.typicode.com"
+
 
 
 def main():
-   employee_id = argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_id = argv[1]
+    
+    all_employees = requests.get(
+        "{}/users/{}".format(
+            base_url,
+            employee_id
+        )
+    ).json()
+    
    
-   all_employees = requests.get(
-       "{}/users/{}".format(
-           base_url,
-           employee_id
-       )
-   ).json()
+    employee_name = all_employees.get('name')
    
-   employee_name = all_employees.get('name')
+    completed_tasks = 0
+    total_tasks = 0
    
-   completed_tasks = 0
-   total_tasks = 0
-   
-   employee_todos = requests.get(
+    employee_todos = requests.get(
        "{}/users/{}/todos/".format(
            base_url,
            employee_id
-       )
-   ).json()
+        )
+    ).json()
 
-   for tasks in employee_todos:
-       total_tasks += 1
-       completed = tasks.get('completed')
+    for tasks in employee_todos: 
+        total_tasks += 1
+        completed = tasks.get('completed')
 
-       if completed == True:
-           completed_tasks += 1          
+        if completed == True:
+            completed_tasks += 1          
 
-   print("Employee {} is done with tasks({}/{}):\
-       ".format(employee_name, completed_tasks, total_tasks))
+    print("Employee {} is done with tasks({}/{}):\
+        ".format(employee_name, completed_tasks, total_tasks))
    
-   print('\n'.join(
-       ['\t ' + tasks.get('title')
-        for tasks in employee_todos
-        if tasks.get('completed') == True
-        ]
-   ))
+    print('\n'.join(
+        ['\t ' + tasks.get('title')
+         for tasks in employee_todos
+         if tasks.get('completed') == True
+         ]
+    ))
 
 
 
