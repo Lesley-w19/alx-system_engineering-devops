bdcbba88f6dd195d4280134dc7810c5cda001440
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-21 14:07:59 +0300

    removed triling spaces and ordered the libraries imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,10 +5,10 @@ for a given employee ID, returns information
 about his/her TODO list progress.
 
 """
-from sys import argv
-import requests
-import urllib
 
+
+import requests
+from sys import argv
 
 
 def main():
@@ -21,13 +21,11 @@ def main():
             employee_id
         )
     ).json()
-    
-   
+
     employee_name = all_employees.get('name')
-   
     completed_tasks = 0
     total_tasks = 0
-   
+
     employee_todos = requests.get(
        "{}/users/{}/todos/".format(
            base_url,
@@ -44,7 +42,7 @@ def main():
 
     print("Employee {} is done with tasks({}/{}):\
         ".format(employee_name, completed_tasks, total_tasks))
-   
+
     print('\n'.join(
         ['\t ' + tasks.get('title')
          for tasks in employee_todos
@@ -53,9 +51,5 @@ def main():
     ))
 
 
-
 if __name__ == "__main__":
     main()
-   
-   
-   
