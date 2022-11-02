fc8c6780a5ecf439cdbbb519b4a18c703820f7e9
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-11-02 15:20:56 +0300

    0. How many subs? ------------ a function that  that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API and
+returns the number of subscribers
+(not active users, total subscribers) for a
+given subreddit.
+If an invalid subreddit is given,
+the function should return 0.
+
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    base_url = 'https://www.reddit.com/'
+    headers = {
+        'User-Agent': 'Mozilla/5.0',
+    }
+
+    try:
+        response = requests.get(
+            url= '{}/r/{}/about.json'.format(base_url,subreddit),
+            headers= headers 
+            );
+
+        if response.status_code != 200:
+            return (0)
+
+        data = response.json()['data']
+        subs = data['subscribers']
+        return subs
+
+    except Exception:
+        return False
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,1 +1,5 @@
 ## 0x16-api_advanced
+
+
+#### TASKS
+0. How many subs?
