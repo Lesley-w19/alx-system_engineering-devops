d18aea88c0f6d609feb9a738e74264f1010464c4
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-11-06 21:05:16 +0300

    changed entire to be one line

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,9 +1,5 @@
 #  configuring your server with Puppet!
 exec {'install':
       provider => shell,
-      command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; 
-      echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i
-      "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me
-      https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;\n\treturn 301;/" 
-      /etc/nginx/sites-available/default ; sudo service nginx start',
+      command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;\n\treturn 301;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
